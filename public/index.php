2f11f8619796d248377a06abd3e9c85cf2c7076a
Bluegnarl <bluegnarl@gmail.com>
2024-07-04 16:09:15 +0200

    First commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = "Je suis un texte";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>PHP Vercel Video</title>
+</head>
+<body>
+    <p><?= $text ?></p>
+    <img src="/assets/img/first.png">
+</body>
+</html>
