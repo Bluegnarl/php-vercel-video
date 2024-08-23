b30c370beb3409e2f4f3f02f7d04205b92dfdf10
Bluegnarl <bluegnarl@gmail.com>
2024-08-23 03:51:02 +0200

    Update multi pages

--- public/help.php
+++ public/help.php
@@ -11,11 +11,11 @@ $data = json_decode(file_get_contents(__DIR__ . "/assets/datas/data.json"), true
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style.css">
-    <title>PHP Vercel Video</title>
+    <title>Help Page</title>
 </head>
 <body>
-    <a href="/help.php"><?= $text ?></a>
-    <img src="/assets/img/second.png">
+    <a href="/"><?= $text ?></a>
+    <img src="/assets/img/first.png">
     <?php foreach ($data as $paragraph) : ?>
         <div>
             <h2><?= $paragraph['title'] ?></h2>
