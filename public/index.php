28e74b78afca1ce372e1fdd4ba0063221f3ba45c
Bluegnarl <bluegnarl@gmail.com>
2024-08-24 15:25:42 +0200

    Reboot

--- public/index.php
+++ public/index.php
@@ -1,18 +1,26 @@
 <?php
 
-$page = $_GET['page'] ?? null;
+$text = "Je suis un texte";
+
 $data = json_decode(file_get_contents(__DIR__ . "/assets/datas/data.json"), true);
 
-switch ($page) {
-    case 'home':
-        require_once __DIR__ . "/pages/home.php";
-        break;
-
-    case 'help':
-        require_once __DIR__ . "/pages/help.php";
-        break;
-    
-    default:
-        require_once __DIR__ . "/pages/home.php";
-        break;
-}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>PHP Vercel Video</title>
+</head>
+<body>
+    <p><?= $text ?></p>
+    <img src="/assets/img/first.png">
+    <?php foreach ($data as $paragraph) : ?>
+        <div>
+            <h2><?= $paragraph['title'] ?></h2>
+            <p><?= $paragraph['text'] ?></p>
+        </div>
+    <?php endforeach ?>
+</body>
+</html>
